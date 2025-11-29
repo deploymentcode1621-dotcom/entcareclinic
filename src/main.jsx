5a1f260d7d42111972d069c094decd0aed7dadbf
AdityaRaut <adityaraut6029@gmail.com>
2025-11-29 20:25:58 +0530

    changes
    by ar

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+// 🚫 Disable all Service Workers (fix popup + network error)
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((reg) => reg.unregister());
+  }).catch((err) => console.log("Service Worker unregister error:", err));
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
